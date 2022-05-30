64bf8f32fe99dd1d0f645dde8884a7aed178eb08
Josue Cordova <jcordova8681@outlook.com>
2022-05-30 18:32:19 -0500

    18:32

---
+++ app/src/main/java/fisei/com/examen_solis_cordova/CActividad2S.java
@@ -0,0 +1,18 @@
+package fisei.com.examen_solis_cordova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class CActividad2S extends AppCompatActivity {
+
+
+    EditText numero;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cactividad2_s);
+    }
+}
