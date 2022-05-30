049416ee51b9c068fff699e13ebb6e508b868df0
Josue Cordova <jcordova8681@outlook.com>
2022-05-30 18:40:21 -0500

    18:40

--- app/src/main/java/fisei/com/examen_solis_cordova/CActividad2S.java
+++ app/src/main/java/fisei/com/examen_solis_cordova/CActividad2S.java
@@ -3,7 +3,10 @@ package fisei.com.examen_solis_cordova;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class CActividad2S extends AppCompatActivity {
 
@@ -14,5 +17,16 @@ public class CActividad2S extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cactividad2_s);
+
+        numero = findViewById(R.id.CNumero_IngresoS);
+
+
+
+    }
+
+    public void IngresarNumero(View view){
+        ArrayList numeros = new ArrayList();
+        numeros.add(numero);
+
     }
 }
