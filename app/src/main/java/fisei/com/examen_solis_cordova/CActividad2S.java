a807e77da9394eeafe4c183a30bd413fdbfc8f41
yJoels <105665177+yJoels@users.noreply.github.com>
2022-05-30 18:50:38 -0500

    18:50

--- app/src/main/java/fisei/com/examen_solis_cordova/CActividad2S.java
+++ app/src/main/java/fisei/com/examen_solis_cordova/CActividad2S.java
@@ -2,7 +2,9 @@ package fisei.com.examen_solis_cordova;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class CActividad2S extends AppCompatActivity {
@@ -15,4 +17,10 @@ public class CActividad2S extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cactividad2_s);
     }
+
+    public void CCerrarS(View view){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
 }
