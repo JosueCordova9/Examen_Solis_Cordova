55504c358596cda96bcb72847b1c0539292f2c2e
Josue Cordova <jcordova8681@outlook.com>
2022-05-30 17:24:56 -0500

    Initial commit

---
+++ app/src/main/java/fisei/com/examen_solis_cordova/MainActivity.java
@@ -0,0 +1,14 @@
+package fisei.com.examen_solis_cordova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
