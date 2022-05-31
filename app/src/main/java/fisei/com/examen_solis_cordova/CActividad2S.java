f55cbbde75950dd7fe19f4c2354a2d32786e47d1
Josue Cordova <jcordova8681@outlook.com>
2022-05-30 19:17:08 -0500

    19:16

--- app/src/main/java/fisei/com/examen_solis_cordova/CActividad2S.java
+++ app/src/main/java/fisei/com/examen_solis_cordova/CActividad2S.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CActividad2S extends AppCompatActivity {
 
@@ -18,6 +21,8 @@ public class CActividad2S extends AppCompatActivity {
     private EditText numero;
     private ListView lista;
     private Button ingresar;
+    private List<Integer> miLista = new ArrayList<Integer>();
+    private ArrayAdapter<Integer> arreglo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +36,22 @@ public class CActividad2S extends AppCompatActivity {
     }
 
     public void CIngresarNumeroS(View view){
-        ArrayList numeros = new ArrayList();
-        numeros.add(numero);
+        switch (view.getId()){
+            case R.id.CbuttonIngresarS: String valor = numero.getText().toString().trim();
+            Integer valor2 = Integer.parseInt(valor);
+            miLista.add(valor2);
+            numero.getText().clear();
+            arreglo = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,miLista);
+            lista.setAdapter(arreglo);
+        }
 
-        lista = numeros;
-
-        Intent intent = new Intent();
-
-        intent.putExtra("numeros",numeros);
-
-        setResult(Activity.RESULT_OK,intent);
-
-
+//        lista = numeros;
+//
+//        Intent intent = new Intent();
+//
+//        intent.putExtra("numeros",numeros);
+//
+//        setResult(Activity.RESULT_OK,intent);
     }
 
     public void CCerrarS(View view){
