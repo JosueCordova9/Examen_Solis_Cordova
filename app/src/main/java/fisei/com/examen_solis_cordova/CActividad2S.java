c5ec628449334ff5a8cadbaf858303bd8fff9ec5
Josue Cordova <jcordova8681@outlook.com>
2022-05-30 19:00:47 -0500

    19:00

--- app/src/main/java/fisei/com/examen_solis_cordova/CActividad2S.java
+++ app/src/main/java/fisei/com/examen_solis_cordova/CActividad2S.java
@@ -2,20 +2,47 @@ package fisei.com.examen_solis_cordova;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class CActividad2S extends AppCompatActivity {
 
 
-    EditText numero;
+    private EditText numero;
+    private ListView lista;
+    private Button ingresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cactividad2_s);
+
+        numero = findViewById(R.id.CNumero_IngresoS);
+        lista = findViewById(R.id.CListaNumerosS);
+        ingresar = findViewById(R.id.CbuttonIngresarS);
+
+    }
+
+    public void CIngresarNumeroS(View view){
+        ArrayList numeros = new ArrayList();
+        numeros.add(numero);
+
+        lista = numeros;
+
+        Intent intent = new Intent();
+
+        intent.putExtra("numeros",numeros);
+
+        setResult(Activity.RESULT_OK,intent);
+
+
     }
 
     public void CCerrarS(View view){
